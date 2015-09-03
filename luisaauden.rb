b366deed6c47c53c665fe19ebb7493c13651109a
Vitorio Paulo <vitoriopaulo@gmail.com>
2015-09-03 19:39:39 -0400

    Vitorio Paulo's Heroku first Committ. ;)

---
+++ luisaauden.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+
+get '/' do
+
+'<body background="imgs/blackbackground.jpeg">
+
+<h1> Luisa Auden - Jewelry Designer </h1>
+
+<p> EN | PT | SP | IT | DE </p>
+
+</body>'
+
+end
---
+++ luisaauden_home.rb
@@ -0,0 +1,3 @@
+require 'sinatra'
+
+require './luisaauden_home.html'
